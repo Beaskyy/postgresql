327867021cfd82f9d38e02839dd86a44fc2271c5
Beaskyy <afeez.akintobi@yahoo.com>
2026-08-13 05:51:32 +0100

    feat: inser mutiple rows

---
+++ part2/03_insert_multiple_rows.sql
@@ -0,0 +1,10 @@
+INSERT INTO products (name, category, price, stock, is_active, sku, description)
+VALUES 
+  ('Laptop', 'Electronics', 2000.00, 5, true, 'LAP-002', 'Macbook Pro 2020'),
+  ('Phone', 'Electronics', 1000.00, 10, false, 'PHO-002', 'Iphone 13'),
+  ('Keyboard', 'Electronics', 50.00, 20, true, 'KEY-003', 'Mechanical keyboard');
+
+  -- SELECT * FROM products;
+  SELECT name, category, price, stock, sku
+  FROM products 
+  WHERE sku IN ('LAP-001', 'PHO-001', 'KEY-001');
